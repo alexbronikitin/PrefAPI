1326b5af6c1ae946024c70dcf67a8e23e5d48c52
alexbronikitin <alexnikitin071209@gmail.com>
2022-09-14 19:22:17 +0500

    Changed dir \ to /. Bad Windows :D

--- PrefAPI/__init__.py
+++ PrefAPI/__init__.py
@@ -2,7 +2,7 @@ from ursinanetworking import *
 from ursinanetworking.easyursinanetworking import *
 import json
 
-config = json.load(open(os.getcwd() + "\config.json"))
+config = json.load(open(os.getcwd() + "/config.json"))
 
 server = UrsinaNetworkingServer(config["ip"], config["port"])
 easy = EasyUrsinaNetworkingServer(server)
